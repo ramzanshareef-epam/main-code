efc07de7edf43d546c56c4d5b811fbba7e73e956
Mohd Ramzan Shareef <mohdramzan_shareef@epam.com>
2025-02-05 14:44:45 +0530

    4th Commit

--- test.js
+++ test.js
@@ -9,3 +9,7 @@ function newFunction() {
 function newFunction2() {
     return "This is the new function which is going to be deleted after git reset if done two times";
 }
+
+function newFunction3() {
+    return "This is the new function which is going to be deleted after git reset if done three times";
+}
